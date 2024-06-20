2796d8d247c92ef0d9af45dbcbebb06eee5c9f96
Moeez Mujahid <moeezmujahid70@gmail.com>
2024-06-20 03:10:59 +0200

    script for tests

---
+++ project/tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# python3 $(dirname "$0")/test.py
+# pytest test.py
+
+python3 $(dirname "$0")/tests/test_etl_pipeline.py
+python3 $(dirname "$0")/tests/test_transform.py
+python3 $(dirname "$0")/tests/test_load.py
