506dca33358dcf075aff9868af3b109d2aec7c88
Moeez Mujahid <moeezmujahid70@gmail.com>
2024-06-22 20:12:41 +0200

    debugging

--- project/tests.sh
+++ project/tests.sh
@@ -2,6 +2,9 @@
 # python3 $(dirname "$0")/test.py
 # pytest test.py
 
-python3 $(dirname "$0")/tests/test_etl_pipeline.py
-python3 $(dirname "$0")/tests/test_transform.py
-python3 $(dirname "$0")/tests/test_load.py
+python3 tests/test_etl_pipeline.py
+
+
+
+#python3 $(dirname "$0")/tests/test_transform.py
+#python3 $(dirname "$0")/tests/test_load.py
